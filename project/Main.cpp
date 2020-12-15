b26d91a163aca4e669dbe9f20344e03dff8b4a88
IonutTDR <62219246+IonutTDR@users.noreply.github.com>
2020-12-15 23:37:17 +0200

    Add files via upload

---
+++ project/Main.cpp
@@ -0,0 +1,34 @@
+#include "UISTL.h"
+#include "UIFile.h"
+#include "Tests.h"
+#include "UISQL.h"
+
+/*MYSQL* conn = mysql_init(NULL);
+
+void sql_connect()
+{
+	if (!conn)
+		cout << "Failed";
+
+	conn = mysql_real_connect(conn, "localhost", "root", "", "mydatabase", 0, NULL, 0);
+
+	if (!conn)
+		cout << "Error";
+}*/
+
+int main()
+{
+
+	myTests();
+
+
+
+	UIFile console;
+	console.run("Aplicatii.txt");
+
+
+
+	
+	
+	return 0;
+}
